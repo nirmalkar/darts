73aa669dfdd6e2a68b711029029a7cd9ea699286
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-07-26 23:21:52 +0530

    PAGES-DASHOBOARD: created

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Donut from "../components/Donut";
+import Navbar from "../components/Navbar";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Navbar />
+      <Donut />
+    </div>
+  );
+};
+export default Dashboard;
