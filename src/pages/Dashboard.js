bae8f3b3c7caeb01eb13a436d0a89e5c7cf6ba3d
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-07-27 21:09:07 +0530

    PAGE-DASHBOARD: imported Drawer and Ishare component

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,12 +2,18 @@ import React from "react";
 
 import Donut from "../components/Donut";
 import Navbar from "../components/Navbar";
+import IShare from "../components/IShares";
+import DrawerComponent from "../components/drawer";
 
 const Dashboard = () => {
   return (
     <div>
       <Navbar />
-      <Donut />
+      <DrawerComponent />
+      <div className="columns">
+        <IShare />
+        <Donut />
+      </div>
     </div>
   );
 };
