269ce731d074a93c8ff3a4df52418193503d5c07
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-07-28 01:52:00 +0530

    REDUX-STORE: share reducer imported

--- src/appRedux/store/index.js
+++ src/appRedux/store/index.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { drawerReducer } from "../reducer/drawerReducer";
+import { shareReducer } from "../reducer/shareReducer";
 
 const initialState = {};
 const reducer = combineReducers({
   drawerVisible: drawerReducer,
+  share: shareReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
