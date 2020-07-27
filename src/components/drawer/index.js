44bbe51b580fc461bfbc1d56f00e06ba8a3ae26d
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-07-27 21:20:05 +0530

    COMPONENT-DRAWER: created and showing input fields inside it

---
+++ src/components/drawer/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Drawer } from "antd";
+import { useSelector, useDispatch } from "react-redux";
+
+import { drawerVisible } from "../../appRedux/action/drawerActions";
+import DrawerFrom from "../forms/DrawerForm";
+
+import "antd/dist/antd.css";
+
+const DrawerComponent = () => {
+  const dispatch = useDispatch();
+  const { isDrawerVisible } = useSelector((state) => state.drawerVisible);
+
+  const handleDrawerVisible = () => {
+    dispatch(drawerVisible(!isDrawerVisible));
+  };
+
+  return (
+    <>
+      <Drawer
+        placement="right"
+        closable={false}
+        visible={isDrawerVisible}
+        width={400}
+      >
+        <span className="is-pulled-right" onClick={handleDrawerVisible}>
+          <i className="fas fa-arrow-right fa-lg" />
+        </span>
+        <br />
+        <br />
+        <br />
+        <DrawerFrom />
+      </Drawer>
+    </>
+  );
+};
+
+export default DrawerComponent;
