3a333c3ebc47e429115d0b6e3ea55acf84bc7c2f
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-07-28 02:02:38 +0530

    DATA-ISHAREDATA: id field added

--- src/constants/data/IShareData.js
+++ src/constants/data/IShareData.js
@@ -1,36 +1,42 @@
 export const ISharesData = [
   {
     name: "ADDR",
+    id: "1",
     price: 50.3,
     quantity: 430,
     investedAmount: "17952.07",
   },
   {
     name: "MFEM",
+    id: "2",
     price: 23.2,
     quantity: 210,
     investedAmount: "4725.84",
   },
   {
     name: "JPEM",
+    id: "3",
     price: 52.5,
     quantity: 328,
     investedAmount: "18597.6",
   },
   {
     name: "KEMQ",
+    id: "4",
     price: 20.4,
     quantity: 801,
     investedAmount: "17811.04",
   },
   {
     name: "KLDW",
+    id: "5",
     price: 32.9,
     quantity: 523,
     investedAmount: "13765.36",
   },
   {
     name: "KOIN",
+    id: "6",
     price: 25.4,
     quantity: 335,
     investedAmount: "8509",
