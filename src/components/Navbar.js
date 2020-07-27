4043712ed4d9e56483543ac2c187b9f1428cc9f9
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-07-27 21:05:09 +0530

    COMPONENT-NAVBAR: connected to redux and dispatched toggle action for show and hide drawer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,15 @@
 import React from "react";
 
+import { useDispatch, useSelector } from "react-redux";
+import { drawerVisible } from "../appRedux/action/drawerActions";
+
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const { isDrawerVisible } = useSelector((state) => state.drawerVisible);
+
+  const handleDrawerVisible = () => {
+    dispatch(drawerVisible(!isDrawerVisible));
+  };
   return (
     <section>
       <nav className="navbar" role="navigation" aria-label="main navigation">
@@ -11,6 +20,9 @@ const Navbar = () => {
             <a className="navbar-item">Home</a>
           </div>
         </div>
+        <span onClick={handleDrawerVisible} className="mr-5 mt-2">
+          <i className="fas fa-bars fa-lg" />
+        </span>
       </nav>
     </section>
   );
