9c015082530bd8d314cf691a3ebb2f5e09898f29
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-07-28 10:46:08 +0530

    COMPONENT-NAVBAR: added dart image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 import { drawerVisible } from "../appRedux/action/drawerActions";
+import Img from "../assets/dart.png";
 
 const Navbar = () => {
   const dispatch = useDispatch();
@@ -13,12 +14,14 @@ const Navbar = () => {
   return (
     <section>
       <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand"></div>
+        <div className="navbar-brand">
+          <span className="navbar-item ml-5">
+            <img src={Img} alt="dart" />
+          </span>
+        </div>
 
         <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start">
-            <a className="navbar-item">Home</a>
-          </div>
+          <div className="navbar-start"></div>
         </div>
         <span onClick={handleDrawerVisible} className="mr-5 mt-2">
           <i className="fas fa-bars fa-lg" />
