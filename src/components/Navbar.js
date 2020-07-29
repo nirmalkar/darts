1349f1d2ffdbbb30287ce2d8372db3b3df45d4b5
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-07-30 02:59:16 +0530

    COMPONENT-NAVBAR: responsiveness issue solved

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,22 +12,35 @@ const Navbar = () => {
     dispatch(drawerVisible(!isDrawerVisible));
   };
   return (
-    <section>
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <span className="navbar-item ml-5">
-            <img src={dartImage} alt="dart" />
-          </span>
+    <div className="hero-head">
+      <nav className="navbar">
+        <div className="container">
+          <div className="navbar-brand">
+            <span className="navbar-item ml-5">
+              <img src={dartImage} alt="dart" />
+            </span>
+            <span
+              onClick={handleDrawerVisible}
+              className="navbar-burger burger"
+              data-target="navbarMenuHeroB"
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+          </div>
+          <div id="navbarMenuHeroB" className="navbar-menu">
+            <div className="navbar-end">
+              <span className="navbar-item">
+                <span onClick={handleDrawerVisible} className="mr-5 mt-2">
+                  <i className="fas fa-bars fa-lg" />
+                </span>
+              </span>
+            </div>
+          </div>
         </div>
-
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start"></div>
-        </div>
-        <span onClick={handleDrawerVisible} className="mr-5 mt-2">
-          <i className="fas fa-bars fa-lg" />
-        </span>
       </nav>
-    </section>
+    </div>
   );
 };
 
