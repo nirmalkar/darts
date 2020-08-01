8cd36a29f639fbd6cc31886d8625b34f0b311d67
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-08-02 00:52:26 +0530

    ACTION_REDUCER_STORE: removed redux for drawer

--- src/appRedux/action/drawerActions.js
+++
@@ -1,7 +0,0 @@
-import { IS_DRAWER_VISIBLE } from "../../constants/actionTypes/drawerActionTypes";
-
-const drawerVisible = (data) => (dispatch) => {
-  dispatch({ type: IS_DRAWER_VISIBLE, payload: data });
-};
-
-export { drawerVisible };
--- src/appRedux/store/index.js
+++ src/appRedux/store/index.js
@@ -1,11 +1,9 @@
 import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
-import { drawerReducer } from "../reducer/drawerReducer";
 import { shareReducer } from "../reducer/shareReducer";
 
 const initialState = {};
 const reducer = combineReducers({
-  drawerVisible: drawerReducer,
   share: shareReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
